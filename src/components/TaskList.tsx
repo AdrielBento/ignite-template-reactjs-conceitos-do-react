6faf56b001ab4daba8e463dafa859cc0d344d1c7
Adriel B <adrielbb15@gmail.com>
2021-03-17 21:43:13 -0300

    implements function to manager a task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,40 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  /**
+   * Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+   */
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle){
+      return;
+    }
+
+    const newTask: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
+  // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.find(task => task.id == id);
+
+    if(!task) {
+      return;
+    }
+
+    task.isComplete = task.isComplete ? false : true;
+
+    setTasks([...tasks.filter(task => task.id != id), task])
   }
 
+  // Remova uma task da listagem pelo ID
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id != id))
   }
 
   return (
